d56fbbda99da9fa6c52ff84b7bf3a978b6d156b7
Dave Hermann <6787333+davehermann@users.noreply.github.com>
2020-07-11 19:22:23 -0400

    Clean up test directory before commencing testing, and randomize directory creation

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,6 @@
 // Node Modules
-const path = require(`path`);
+const path = require(`path`),
+    fs = require(`fs`).promises;
 
 // NPM Modules
 const expect = require(`chai`).expect;
@@ -9,11 +10,31 @@ const assert = require(`assert`);
 const { EnsurePathForFile, ReadSubDirectories } = require(`../pathgen`);
 
 describe(`Utilities`, function() {
+    before(async function() {
+        const testPath = path.join(process.cwd(), `tests`, `generated`);
+        const testDirectories = await ReadSubDirectories(testPath);
+
+        // Get the lowest-level directories, and recurse up to delete
+        const removeDirectories = async (fsObjects) => {
+            while (fsObjects.length > 0) {
+                const nextObject = fsObjects.shift();
+
+                if (nextObject.isDirectory) {
+                    await removeDirectories(nextObject.items);
+
+                    fs.rmdir(nextObject.objectPath);
+                }
+            }
+        };
+
+        removeDirectories(testDirectories);
+    });
+
     describe(`ensurePath`, function() {
         it(`should create directories`, function() {
             return assert.doesNotReject(() =>
                 // Promise.reject(`failed`)
-                EnsurePathForFile(path.join(process.cwd(), `tests`, `generated`, `example`, `1`, `dummyfile.txt`))
+                EnsurePathForFile(path.join(process.cwd(), `tests`, `generated`, `example`, `${Math.floor(Math.random() * 100000)}`, `dummyfile.txt`))
             );
         });
 
