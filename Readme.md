70f26d6dc36d179ea267714905e3c6fe6a9aed14
Dave Hermann <6787333+davehermann@users.noreply.github.com>
2020-11-04 12:13:37 -0500

    Provide additional details on type data for the interfaces

--- Readme.md
+++ Readme.md
@@ -27,4 +27,4 @@ Reads all file system objects in a directory, and returns `fs.stat()` on each as
 #### ReadOptions class
 | Property | Type | Required | Notes |
 | -------- | :--: | :------: | ----- |
-| returnProperties | string[] | no | List of `fs.Stats` properties to return<ul><li>Returns all properties by default</li></ul>*See documentation for your version of NodeJS for available properties* |
+| returnProperties | string[] | no | List of `fs.Stats` properties to return<ul><li>Returns all properties by default</li></ul>*See [NodeJS FS Stats class documentation](https://nodejs.org/dist/latest/docs/api/fs.html#fs_class_fs_stats) for your NodeJS version to see available properties* |
--- dist/readSubDirectory.d.ts
+++ dist/readSubDirectory.d.ts
@@ -1,11 +1,24 @@
+/** Options for reading subdirectories */
 interface IReadOptions {
+    /**
+     * List of properties to return in the data object from NodeJS fs module's `Stats` class
+     *
+     * @remarks
+     * See the [NodeJS FS Stats class documentation](https://nodejs.org/dist/latest/docs/api/fs.html#fs_class_fs_stats) for your NodeJS version to get a list of available properties
+     */
     returnProperties: Array<string>;
 }
+/** Directory data */
 interface IDirectoryObject {
+    /** Name of the object within the current path */
     fsName: string;
+    /** Absolute path to the object */
     objectPath?: string;
+    /** Is the object a directory? */
     isDirectory?: boolean;
+    /** The FS Stats class for the object, or the sub-set of properties in the IReadOptions parameters */
     stats?: Object;
+    /** For a directory, the list of file system objects within that directory */
     items?: Array<IDirectoryObject>;
 }
 /**
