b7570d905e575e4dd16c3dc710651fb8877697fe
Dave Hermann <6787333+davehermann@users.noreply.github.com>
2020-11-13 08:37:42 -0500

    Add support for removing files/directories

--- src/fsutilities.ts
+++ src/fsutilities.ts
@@ -1,10 +1,12 @@
 // Utility Modules
 import { EnsurePathForFile } from "./ensurePath";
 import { IDirectoryObject, IReadOptions, ReadSubDirectories } from "./readSubDirectory";
+import { RemovePath } from "./removePath";
 
 export {
     IDirectoryObject,
     IReadOptions,
     EnsurePathForFile,
     ReadSubDirectories,
+    RemovePath,
 };
---
+++ src/removePath.ts
@@ -0,0 +1,42 @@
+import { promises as fs } from "fs";
+
+import { ReadSubDirectories, IDirectoryObject } from "./readSubDirectory";
+
+/**
+ * Delete a file or directory
+ * @param fileSystemPath - absolute path to the file system object to be deleted
+ * @param verbose - log delete actions to the console
+ */
+async function removePath(fileSystemPath: string, verbose = false): Promise<void> {
+    if (verbose)
+        console.log(`Remove ${fileSystemPath}`);
+
+    // Get path contents
+    const fsObjects = await ReadSubDirectories(fileSystemPath, { returnProperties: [] });
+
+    await removeFileSystemObjects(fsObjects, verbose);
+}
+
+async function removeFileSystemObjects(fsObjects: Array<IDirectoryObject>, verbose: boolean): Promise<void> {
+    while (fsObjects.length > 0) {
+        const nextObject = fsObjects.shift();
+
+        if (nextObject.isDirectory) {
+            await removeFileSystemObjects(nextObject.items, verbose);
+
+            if (verbose)
+                console.log(`Deleting Directory: ${nextObject.objectPath}`);
+
+            await fs.rmdir(nextObject.objectPath);
+        } else {
+            if (verbose)
+                console.log(`Deleting File: ${nextObject.objectPath}`);
+
+            await fs.unlink(nextObject.objectPath);
+        }
+    }
+}
+
+export {
+    removePath as RemovePath,
+};
